22ae5c756bb553546912a82d209fc76acd668694
Liam <liamnahuelromero@gmail.com>
2025-05-29 11:37:37 -0300

    Se agregan todos los apuntes hasta la fecha

--- 2. introduccionJavascript.js
+++ 2. introduccionJavascript.js
@@ -2,14 +2,14 @@
 
 let variablesYTiposDeDatos = "Una variable es un espacio en la memoria que se usa para guardar un dato que puede cambiar durante la ejecución del programa. Cada variable tiene un nombre, un tipo de dato (como números, textos, etc.) y un valor.";
 
-// Operadores Lógicos
+// -- -- -- Operadores Lógicos -- -- --
 let operadoresLogicos = {
     and : "&&",
     or : "||",
     not : "!"
 };
 
-// Funciones
+// -- -- -- Funciones -- -- --
 let funciones = {
     definición: "Es un bloque de código reutilizable que se ejecuta cuando es llamado. Permite organizar el código, evitar repeticiones y facilitar tareas complejas.",
     ejemploDe: "función saludar"
@@ -21,11 +21,11 @@ function saludar(){
 }; 
 saludar();
 
-// Return
+// -- -- -- Return -- -- --
 let palabraReturn = "el return se utiliza dentro de una funcion para devolver un valor al lugar donde fue llamado."; 
 
 
-// Condicionales
+// -- -- -- Condicionales -- -- --
 let condicionales = {
     estructuraIfElse : "se utiliza para tomar decisiones en el codigo. Permite ejecutar diferentes bloques de código según si una condición es verdadera o falsa. (Evalua condiciones booleanas (True/False)).",
     instruccionElseIf : "Se usa cuando queremos evaluar multiples condiciones despues de un if. Permite comprobar varias opciones en orden, y ejecutar el primer bloque cuyo resultado sea verdadero.",
@@ -46,7 +46,7 @@ let condicionalesAnidados = "Los condicionales anidados son estructuras if, else
 
 let ifTernario = "El operador ternario es una forma abreviada de escribir una estructura if/else. Se usa para evaluar una condición y devolver un valor u otro en una sola línea.";
 
-// Switch
+// -- -- -- Switch -- -- --
 let estructuraSwitch = {
     definición: "Se usa para evaluar una expresión y ejecutar diferentes bloques de código según el valor que tome esa expresión. Es una alternativa al uso de múltiples if/else if.",
 //La diferencia entre switch y if/else if, es que el if se usa cuando queremos evaluar condiciones complejas o diferentes tipos de comparaciones. Mientras que switch se usa cuando queremos comparar una misma variable contra varios valores exactos. Es más ordenado cuando hay muchas opciones posibles para un solo valor.
--- 4. javascriptAvanzado.js
+++ 4. javascriptAvanzado.js
@@ -1,6 +1,43 @@
 // JavaScript Avanzados
 
+// -- -- -- Matrices -- -- --
 let matrices = {
-    definición : "",
-    sintaxis : ""
+    definición : "Una matriz se representa como un array de arrays, es decir, un array donde cada elemento también es un array. Esto permite representar estructura de filas y columnas, como una tabla.",
+    sintaxisDe : "matrices"
 };
+
+let matriz = [
+    [1,2,3],
+    [4,5,6]
+]
+//              fila / columna
+console.log(matriz[0][1]);
+
+// -- -- -- Tipos de Funciones -- -- --
+let tiposDeFunciones = {
+    funcionesDeclaradas : "Son las funciones tradicionales, definidas con la palabra clave function y un nombre. La principal caracteristica de estas funciones es que tienen hoisting, es decir, pueden ser llamadas antes de su declaración en el código, porque JavaScript las eleva al principio del archivo durante la ejecución.",
+
+    funcionesExpresadas : "Son funciones que se definen dentro de una variable, no tienen hoisting, lo que significa que no se pueden usar antes de ser declaradas. Se crean en tiempo de ejecución.",
+
+    arrowFunctions : "son una forma mas moderna y concisa de escribir funciones. Se usan mucho cuando se necesita una función rápida o en callbacks. No tienen hoisting, asi que no se pueden usar antes de definirlas"
+};
+
+// -- -- -- Callbacks -- -- --
+let callbacks = {
+    definición : "Es una función que se pasa como argumento a otra función, y que luego se ejecuta denro de esa función en un momento determinado",
+    caracterisiticas : {
+        1 : "Ejecutan código después de que otro haya terminado",
+        2 : "Trabaja con tareas asincrónicas, como peticiones a un servidores, temporizadores, etc.",
+        3 : "Reutliza funciones de manera flexible"
+    },
+    sintaxisDe : "callbacks"
+};
+
+function sumar(a, b, callback) {
+  const resultado = a + b;
+  callback(resultado);
+}
+
+sumar(5, 3, (resultado) => {
+  console.log("El resultado es: " + resultado);
+});
